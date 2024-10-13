a7b27e2bacd53378c81c8a629b13129850a989b6
r0bVious <robert.spano@gmail.com>
2024-10-13 20:38:42 +0900

    Initial commit - test success

--- index.js
+++ index.js
@@ -1,20 +1,45 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const multerUpload = multer({ dest: "uploads/" });
+const cors = require("cors");
+require("dotenv").config();
 
-var app = express();
+const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+const getMetadata = (req, res) => {
+  multerUpload.single("upfile")(req, res, (err) => {
+    if (err) {
+      return res.status(500).json({ error: err.message });
+    }
 
+    const { originalname, mimetype, size } = req.file;
+    res.json({
+      name: originalname,
+      type: mimetype,
+      size: size,
+    });
+  });
+};
 
+app.post("/api/fileanalyse", getMetadata);
 
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT || 3000);
+  })
+  .then(
+    console.log(
+      "Connected to MongoDB + Backend listening on port",
+      process.env.PORT
+    )
+  )
+  .catch((error) => console.log(error));
